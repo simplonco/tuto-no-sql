c3a57534a48494e759a4b2e03196f19bcaa8723f
Harysson <hjeanbaptiste@simplon.co>
2018-02-09 16:18:20 +0100

    ajout du create

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,13 @@ router.get('/', (req, res, next) => {
         .catch(err=> next(err));
 });
 
+router.post('/', (req, res, next) => {
+    let user = new User({
+        username:req.body.username
+    });
+    user.save().then((user)=>{
+        return res.status(200).json({message: 'User successfully created: ' , user:user});
+    }).catch((err)=>{next(err)});
+});
+
 module.exports = router;
